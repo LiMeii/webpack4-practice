9ba112d19b1966265634469f9ad4e2c4d7bbc735
LiMeii <mei90s.lee@gmail.com>
2019-08-20 18:37:09 +0800

    update code

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,4 +1,12 @@
 const merge = require("webpack-merge")
+const path = require('path');
 const webpackBaseConfig = require("./webpack.common.config.js")
 
-module.exports = merge(webpackBaseConfig, {})
+module.exports = merge(webpackBaseConfig, {
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 4207,
+        hot: true
+    }
+})
